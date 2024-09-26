826de0858b904c94692624cff58da8ced143a1ac
James King <james@mercury.com>
2024-09-26 14:02:20 -0400

    Add flake.nix
    We'll use Nix/flakes to manage the development environment.
    
    Adds the `zig` compiler along with `git` and the Haskell toolchain.

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{ inputs.all-cabal-hashes = {
+    url = "github:commercialhaskell/all-cabal-hashes/hackage";
+
+    flake = false;
+  };
+
+  outputs = { all-cabal-hashes, flake-utils, nixpkgs, self }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        config.allowUnfree = true;
+
+        pkgs = import nixpkgs {
+          inherit config system;
+
+          overlays = [ self.overlays.default ];
+        };
+
+      in
+        { packages.default = pkgs.haskellPackagesCustom.tigerbeetle-hs;
+
+          devShells.default = pkgs.haskellPackagesCustom.shellFor {
+            packages = hpkgs: [
+              hpkgs.tigerbeetle-hs
+            ];
+
+            nativeBuildInputs = [
+              pkgs.zig
+              pkgs.git
+            ];
+
+            withHoogle = true;
+
+            doBenchmark = true;
+          };
+        }
+    ) // {
+      overlays.default = self: super: {
+        inherit all-cabal-hashes;
+
+        haskellPackagesCustom = self.haskellPackages.override (old: {
+          overrides =
+            let
+              hlib = self.haskell.lib.compose;
+            in
+              self.lib.composeManyExtensions [
+                (hlib.packageSourceOverrides {
+                  tigerbeetle-hs = ./.;
+                })
+
+                (hlib.packagesFromDirectory {
+                  directory = ./dependencies;
+                })
+
+                (hself: hsuper: {
+                })
+              ];
+        });
+      };
+    };
+}
